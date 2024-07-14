aa711da95d7f2bbfeb04c3cab3d26f3308c834bd
DaDaDei <yuanhaiyuedadadei@gmail.com>
2024-07-14 10:08:32 -0400

    security update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -106,12 +106,18 @@ const Contact = () => {
   
 
     emailjs
-      .sendForm(
+      /*.sendForm(
         "service_a1qzj8t",
         "template_8tcgfx2",
         ref.current,
         "LkrdHRjNmzQZdprkm"
-      )
+      )*/
+      .sendForm(
+          process.env.REACT_APP_EMAILJS_SERVICE_ID,
+          process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+          ref.current,
+          process.env.REACT_APP_EMAILJS_USER_ID
+        )
       .then(
         (result) => {
           console.log(result.text);
